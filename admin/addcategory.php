76859dd907333efefd59dae3f3000d40b08730fe
vuonghaphan <vuongph62@wru.vn>
2020-03-12 20:03:30 +0700

    init project

---
+++ admin/addcategory.php
@@ -0,0 +1,60 @@
+
+	<!--/main-->
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#"><svg class="glyph stroked home">
+							<use xlink:href="#stroked-home"></use>
+						</svg></a></li>
+				<li class="active">Danh mục</li>
+			</ol>
+		</div>
+		
+
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Quản lý danh mục</h1>
+			</div>
+		</div>
+		<!--/.row-->
+		<div class="row">
+			<div class="col-md-12">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-5">
+
+								<div class="form-group">
+									<label for="">Danh mục cha:</label>
+									<select class="form-control" name="" id="">
+										<option>----ROOT----</option>
+										<option>Nam</option>
+										<option>---|Áo khoác nam</option>
+										<option>---|---|Áo khoác nam</option>
+										<option>Nữ</option>
+										<option>---|Áo khoác nữ</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<label for="">Tên Danh mục</label>
+									<input type="text" class="form-control" name="name" id="" placeholder="Tên danh mục mới">
+
+									<div class="alert bg-danger" role="alert">
+										<svg class="glyph stroked cancel">
+											<use xlink:href="#stroked-cancel"></use>
+										</svg>Tên danh mục đã tồn tại!<a href="#" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+									</div>
+								</div>
+								<button type="submit" class="btn btn-primary">Thêm danh mục</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/.col-->
+
+
+		</div>
+		<!--/.row-->
+	</div>
+	<!--/.main-->
---
+++ admin/category.php
@@ -0,0 +1,82 @@
+
+	<!--/main-->
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#"><svg class="glyph stroked home">
+							<use xlink:href="#stroked-home"></use>
+						</svg></a></li>
+				<li class="active">Danh mục</li>
+			</ol>
+		</div>
+		
+
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Quản lý danh mục</h1>
+			</div>
+		</div>
+		<!--/.row-->
+		<div class="row">
+			<div class="col-md-12">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-7">
+								<div class="alert bg-success" role="alert">
+									<svg class="glyph stroked checkmark">
+										<use xlink:href="#stroked-checkmark"></use>
+									</svg> Đã thêm danh mục thành công! <a href="#" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+								</div>
+								<h3 style="margin: 0;"><strong>Phân cấp Menu</strong></h3>
+								<div class="vertical-menu">
+									<div class="item-menu active">Danh mục </div>
+									<div class="item-menu"><span>Nam</span>
+										<div class="category-fix">
+											<a class="btn-category btn-primary" href="editcategory.html"><i class="fa fa-edit"></i></a>
+											<a class="btn-category btn-danger" href="#"><i class="fas fa-times"></i></i></a>
+
+										</div>
+									</div>
+									<div class="item-menu"><span>---|Áo khoác Nam</span>
+										<div class="category-fix">
+											<a class="btn-category btn-primary" href="editcategory.html"><i class="fa fa-edit"></i></a>
+											<a class="btn-category btn-danger" href="#"><i class="fas fa-times"></i></i></a>
+
+										</div>
+									</div>
+									<div class="item-menu"><span>---|---|Áo khoác Nam (Dành cho việc mở rộng)</span>
+										<div class="category-fix">
+											<a class="btn-category btn-primary" href="editcategory.html"><i class="fa fa-edit"></i></a>
+											<a class="btn-category btn-danger" href="#"><i class="fas fa-times"></i></i></a>
+
+										</div>
+									</div>
+									<div class="item-menu"><span>Nữ</span>
+										<div class="category-fix">
+											<a class="btn-category btn-primary" href="editcategory.html"><i class="fa fa-edit"></i></a>
+											<a class="btn-category btn-danger" href="#"><i class="fas fa-times"></i></i></a>
+
+										</div>
+									</div>
+									<div class="item-menu"><span>---|Áo khoác Nữ</span>
+										<div class="category-fix">
+											<a class="btn-category btn-primary" href="editcategory.html"><i class="fa fa-edit"></i></a>
+											<a class="btn-category btn-danger" href="#"><i class="fas fa-times"></i></i></a>
+
+										</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/.col-->
+
+
+		</div>
+		<!--/.row-->
+	</div>
+	<!--/.main-->
